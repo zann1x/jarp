9259be54e487b4244344e53506f97ebe7155bafe
Lukas Zanner <git@lukaszanner.de>
2019-12-24 11:27:50 +0100

    Make window usable on linux

--- jarp/Source/Platform/Linux/LinuxWindow.cpp
+++ jarp/Source/Platform/Linux/LinuxWindow.cpp
@@ -5,6 +5,10 @@
 #include "LinuxWindow.h"
 
 #include "jarp/Core.h"
+#include "jarp/Events/ApplicationEvent.h"
+#include "jarp/Events/EventBus.h"
+#include "jarp/Events/KeyEvent.h"
+#include "jarp/Events/MouseEvent.h"
 
 namespace jarp {
 
@@ -17,6 +21,10 @@ namespace jarp {
 
     LinuxWindow::LinuxWindow(const WindowProperties& Properties)
     {
+		m_Data.Title = Properties.Title;
+		m_Data.Width = Properties.Width;
+		m_Data.Height = Properties.Height;
+
         if (!bIsSDLInitialized)
 		{
 			int SDLInitResult = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
@@ -35,6 +43,82 @@ namespace jarp {
 		SDL_DestroyWindow(m_Window);
 		SDL_Quit();
     }
+
+	xcb_connection_t* LinuxWindow::GetNativeConnectionHandle() const
+	{
+		
+	}
+
+	xcb_window_t LinuxWindow::GetNativeWindowHandle() const
+	{
+		
+	}
+
+	void LinuxWindow::Update(uint32_t deltaTime)
+	{
+		SDL_Event sdlEvent;
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+				case SDL_QUIT:
+				{
+					WindowClosedEvent event;
+					EventBus::Get().Dispatch(event);
+					break;
+				}
+				case SDL_WINDOWEVENT:
+				{
+					switch (sdlEvent.window.event)
+					{
+						case SDL_WINDOWEVENT_MINIMIZED:
+						{
+							m_bIsWindowMinimized = true;
+							WindowMinimizedEvent event;
+							EventBus::Get().Dispatch(event);
+							break;
+						}
+						case SDL_WINDOWEVENT_RESTORED:
+						{
+							m_bIsWindowMinimized = false;
+							WindowRestoredEvent event;
+							EventBus::Get().Dispatch(event);
+							break;
+						}
+						case SDL_WINDOWEVENT_SIZE_CHANGED:
+						{
+							m_bIsFramebufferResized = true;
+							WindowResizedEvent event(sdlEvent.window.data1, sdlEvent.window.data2);
+							EventBus::Get().Dispatch(event);
+							break;
+						}
+					}
+				}
+				case SDL_MOUSEMOTION:
+				{
+					int x, y;
+					SDL_GetMouseState(&x, &y);
+					MouseMovedEvent event(x, y);
+					EventBus::Get().Dispatch(event);
+					break;
+				}
+				case SDL_KEYDOWN:
+				{
+					JARP_CORE_INFO("Key pressed: {0}", sdlEvent.key.keysym.sym);
+					KeyPressedEvent event(sdlEvent.key.keysym.sym);
+					EventBus::Get().Dispatch(event);
+					break;
+				}
+			}
+		}
+	}
+
+	std::pair<int, int> LinuxWindow::GetFramebufferSize() const
+	{
+		int width, height;
+		SDL_Vulkan_GetDrawableSize(m_Window, &width, &height);
+		return { width, height };
+	}
 
 }
 
--- jarp/Source/Platform/Linux/LinuxWindow.h
+++ jarp/Source/Platform/Linux/LinuxWindow.h
@@ -25,15 +25,24 @@ namespace jarp {
         virtual void Update(uint32_t deltaTime) override;
 
 		virtual std::pair<int, int> GetFramebufferSize() const override;
-		virtual int GetWidth() const override;
-		virtual int GetHeight() const override;
-		virtual const char* GetTitle() const override;
-		virtual void* GetNativeWindow() const override;
+		virtual int GetWidth() const override { return m_Data.Width; };
+		virtual int GetHeight() const override { return m_Data.Height; };
+		virtual const char* GetTitle() const override { return m_Data.Title; };
+		virtual void* GetNativeWindow() const override { return m_Window; }
 
-		virtual bool IsMinimized() const override;
+		virtual bool IsMinimized() const override { return m_bIsWindowMinimized; }
 
     private:
         SDL_Window* m_Window;
+        bool m_bIsFramebufferResized = false;
+        bool m_bIsWindowMinimized = false;
+
+        struct SWindowData
+		{
+			const char* Title;
+			int Width;
+			int Height;
+		} m_Data;
     };
 
 }
