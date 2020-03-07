ebe7efcae0f6a9cc136b865db5db82956420d20a
Lukas Zanner <git@lukaszanner.de>
2020-03-07 20:05:14 +0100

    Add basic rendering loop

--- jarp/Source/main.c
+++ jarp/Source/main.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <SDL.h>
 
 int main(int argc, char** argv)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	// Initialize SDL
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
 	SDL_Window* window = SDL_CreateWindow("Hello world", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xff, 0xdd, 0xaa));
-	SDL_UpdateWindowSurface(window);
-	SDL_Delay(5000);
+	SDL_SetWindowResizable(window, SDL_TRUE);
+
+	uint32_t currentFPSTime = SDL_GetTicks();
+	uint32_t lastFPSTime = currentFPSTime;
+	uint32_t frames = 0;
+
+	bool isRunning = true;
+	while (isRunning == true)
+	{
+		currentFPSTime = SDL_GetTicks();
+		++frames;
+		if (currentFPSTime > lastFPSTime + 1000)
+		{
+			printf("%d fps\n", frames);
+			lastFPSTime = currentFPSTime;
+			frames = 0;
+		}
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			}
+		}
+
+		SDL_Surface* surface = SDL_GetWindowSurface(window);
+		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xff, 0xdd, 0xaa));
+		SDL_UpdateWindowSurface(window);
+	}
+
+	// Quit SDL
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
