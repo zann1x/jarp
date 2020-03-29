29171c0ff4af5f06ee0532d9ce36f413993e9da1
Lukas Zanner <git@lukaszanner.de>
2020-03-29 21:52:03 +0200

    Add dllexport define to general api types include

--- jarp/Source/jarp/api_types.h
+++ jarp/Source/jarp/api_types.h
@@ -4,3 +4,15 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+#if JARP_PLATFORM_WIN32
+	#ifdef __cplusplus
+		#define API extern "C"
+	#else
+		#define API __declspec(dllexport)
+	#endif
+#elif JARP_PLATFORM_LINUX
+	#define API
+#else
+	#define API
+#endif
