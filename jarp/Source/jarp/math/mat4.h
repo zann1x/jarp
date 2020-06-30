38facec5a97f4a10466d3f3d8322c487586ff6ba
Lukas Zanner <git@lukaszanner.de>
2020-06-30 08:39:18 +0200

    Fix wrong matrix calculations and a clip matrix
    The clip matrix is needed to clip the OpenGL based matrix with the
    y-axis in positive direction and z backwards to the Vulkan coordinate
    system with y downwards and z forward.
    The look_at matrix had a calculation error and was corrected based on
    a blog post from scratchapixel.

--- jarp/Source/jarp/math/mat4.h
+++ jarp/Source/jarp/math/mat4.h
@@ -84,6 +84,8 @@ inline static Mat4f math_mat4f_rotation(float angle) {
 /*
 ====================
 math_mat4f_orthographic
+
+See https://solarianprogrammer.com/2013/05/22/opengl-101-matrices-projection-view-model/
 ====================
 */
 inline static Mat4f math_mat4f_orthographic(float left, float right, float bottom, float top, float near, float far) {
@@ -95,42 +97,62 @@ inline static Mat4f math_mat4f_orthographic(float left, float right, float botto
 
     result.elements[2 + 2 * 4] = -2.0f / (far - near);
 
-    result.elements[0 + 3 * 4] = -(right + left) / (right - left);
-    result.elements[1 + 3 * 4] = -(top + bottom) / (top - bottom);
-    result.elements[2 + 3 * 4] = -(far + near) / (far - near);
+    result.elements[0 + 3 * 4] = -((right + left) / (right - left));
+    result.elements[1 + 3 * 4] = -((top + bottom) / (top - bottom));
+    result.elements[2 + 3 * 4] = -((far + near) / (far - near));
 
-    // TODO?: Convert projection matrix to Vulkan coordinate system
-    //        https://matthewwellings.com/blog/the-new-vulkan-coordinate-system/
-    
     return result;
 }
 
 /*
 ====================
 math_mat4f_look_at
+
+See https://www.scratchapixel.com/lessons/mathematics-physics-for-computer-graphics/lookat-function
 ====================
 */
 inline static Mat4f math_mat4f_look_at(const Vec3f eye, const Vec3f target, const Vec3f up) {
     Mat4f result = math_mat4f_identity();
 
     Vec3f zaxis = math_vec3f_normalize(math_vec3f_substract(eye, target)); // forward
-    Vec3f xaxis = math_vec3f_normalize(math_vec3f_cross(up, zaxis)); // right
-    Vec3f yaxis = math_vec3f_normalize(math_vec3f_cross(zaxis, xaxis)); // up
+    //Vec3f xaxis = math_vec3f_normalize(math_vec3f_cross(up, zaxis)); // right
+    Vec3f xaxis = math_vec3f_cross(math_vec3f_normalize(up), zaxis); // right
+    //Vec3f yaxis = math_vec3f_normalize(math_vec3f_cross(zaxis, xaxis)); // up
+    Vec3f yaxis = math_vec3f_cross(zaxis, xaxis); // up
 
     result.elements[0 + 0 * 4] = xaxis.x;
     result.elements[0 + 1 * 4] = xaxis.y;
     result.elements[0 + 2 * 4] = xaxis.z;
-    result.elements[0 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(xaxis), eye);
+    //result.elements[0 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(xaxis), eye);
 
     result.elements[1 + 0 * 4] = yaxis.x;
     result.elements[1 + 1 * 4] = yaxis.y;
     result.elements[1 + 2 * 4] = yaxis.z;
-    result.elements[1 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(yaxis), eye);
+    //result.elements[1 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(yaxis), eye);
 
     result.elements[2 + 0 * 4] = zaxis.x;
     result.elements[2 + 1 * 4] = zaxis.y;
     result.elements[2 + 2 * 4] = zaxis.z;
-    result.elements[2 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(zaxis), eye);
+    //result.elements[2 + 3 * 4] = math_vec3f_dot(math_vec3f_negate(zaxis), eye);
+
+    result.elements[3 + 0 * 4] = target.x;
+    result.elements[3 + 1 * 4] = target.y;
+    result.elements[3 + 2 * 4] = target.z;
 
     return result;
+}
+
+/*
+====================
+math_mat4f_clip
+
+See https://matthewwellings.com/blog/the-new-vulkan-coordinate-system/
+====================
+*/
+inline static Mat4f math_mat4f_clip(const Mat4f mat) {
+    Mat4f clip = math_mat4f_identity();
+    clip.elements[1 + 1 * 4] = -1.0f;
+    clip.elements[2 + 2 * 4] = 0.5f;
+    clip.elements[2 + 3 * 4] = 0.5f;
+    return math_mat4f_multiply(clip, mat);
 }
