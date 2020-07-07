57e0176dc6f3d4bb1b86709cf9f05b644bc27ee8
Lukas Zanner <git@lukaszanner.de>
2020-07-07 08:34:45 +0200

    Merge input files

--- jarp/Source/jarp/input/input.h
+++ jarp/Source/jarp/input/input.h
@@ -1,7 +1,28 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#include "keys.h"
+enum EButton {
+    BUTTON_LEFT = SDL_BUTTON_LEFT,
+    BUTTON_MIDDLE = SDL_BUTTON_MIDDLE,
+    BUTTON_RIGHT = SDL_BUTTON_RIGHT,
+    BUTTON_BACK = SDL_BUTTON_X1,
+    BUTTON_FORWARD = SDL_BUTTON_X2
+};
+
+enum EKey {
+    KEY_UNKNOWN = SDL_SCANCODE_UNKNOWN,
+
+    KEY_W = SDL_SCANCODE_W,
+    KEY_A = SDL_SCANCODE_A,
+    KEY_S = SDL_SCANCODE_S,
+    KEY_D = SDL_SCANCODE_D,
+    KEY_SPACE = SDL_SCANCODE_SPACE,
+
+    KEY_LEFT = SDL_SCANCODE_LEFT,
+    KEY_RIGHT = SDL_SCANCODE_RIGHT,
+
+    KEY_COUNT = SDL_NUM_SCANCODES
+};
 
 extern int input_mouse_x;
 extern int input_mouse_y;
